dd888aa34b22e7237bcd6f2ff2f40ffe2f411dcc
nawaf al-mehdhar <108904563+nawaf-dh@users.noreply.github.com>
2024-02-02 17:58:34 +0300

    Exercise 1.5 submission
    1.5: course information step5

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -1,23 +1,25 @@
 const App = () => {
-  const course = 'Half Stack application development'
-  const parts = [
-    {
-      name: 'Fundamentals of React',
-      exercises: 10
-    },
-    {
-      name: 'Using props to pass data',
-      exercises: 7
-    },
-    {
-      name: 'State of a component',
-      exercises: 14
-    }
-  ]
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
 
   const Header = (props) => {
     return <>
-      <h1>{props.course}</h1>
+      <h1>{props.name}</h1>
     </>
   }
   const Part = (props) => {
@@ -37,7 +39,6 @@ const App = () => {
     </>
   }
   const Total = (props) => {
-    console.log(props)
     return <>
       <p>
        Number of exercises {props.parts[0].exercises + props.parts[1].exercises + props.parts[2].exercises}
@@ -48,10 +49,10 @@ const App = () => {
 
   return (
     <div>
-      <Header course={course} />
+      <Header name={course.name} />
       <Content 
-      parts={parts} />
-      <Total parts={parts} />
+      parts={course.parts} />
+      <Total parts={course.parts} />
     </div>
   )
 }
