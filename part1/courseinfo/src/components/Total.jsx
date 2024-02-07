9cc71f393ba8f0122488dd3a433f70d8996587a5
nawaf al-mehdhar <108904563+nawaf-dh@users.noreply.github.com>
2024-02-07 07:42:41 +0300

    Exercises (2.1 - 2.5) submission
    Course information steps (6 - 10)

---
+++ part1/courseinfo/src/components/Total.jsx
@@ -0,0 +1,10 @@
+const Total = ({ parts }) => {
+    return <>
+        <p>
+        <strong>Total of {parts.reduce((a, b) => a + b.exercises, 0)} exercises</strong>
+        </p> 
+
+    </>
+}
+
+export default Total
